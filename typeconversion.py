10e8362b47d50ddb230e06fac9864e0f7cf6def7
developer-ajay <developer.ajay618@gmail.com>
2024-10-02 12:18:40 +0530

    type conversion in python

---
+++ typeconversion.py
@@ -0,0 +1,4 @@
+birth_year = input("Enter your birth year ")
+print(type(birth_year))
+age = 2024 - int (birth_year)
+print(age)
---
+++ weight_conversion.py
@@ -0,0 +1,3 @@
+weight_in_pounds = input("Enter the weight in pounds : ")
+weight_in_kilos = float(weight_in_pounds) * 0.453592
+print(round(weight_in_kilos,2))
